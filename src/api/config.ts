0e3096cfc9ab10706456dc5fb27ad6fec39e3dc9
Narges Jahromi <nrgs.jahromi@gmail.com>
2024-08-22 04:57:12 +0330

    add logout API

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,29 @@
+// import axios, { AxiosError } from "axios";
+// import { API_BASE_URL } from "../vars/env";
+// import { setInterceptors } from "./interceptors";
+
+// export const fetcher = axios.create({
+//   baseURL: API_BASE_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// setInterceptors(fetcher);
+
+// export type ApiErrorData = {
+//   detail: {
+//     non_field_error: string[];
+//     [key: string]: string[];
+//   };
+// };
+
+// export type ApiError = AxiosError<ApiErrorData>;
+
+
 import axios, { AxiosError } from "axios";
 import { API_BASE_URL } from "../vars/env";
-import { setInterceptors } from "./interceptors";
+
 
 export const fetcher = axios.create({
   baseURL: API_BASE_URL,
@@ -9,7 +32,18 @@ export const fetcher = axios.create({
   },
 });
 
-setInterceptors(fetcher);
+fetcher.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export type ApiErrorData = {
   detail: {
