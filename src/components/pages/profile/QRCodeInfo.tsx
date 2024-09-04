6c9e7506acd47ebc51b385f68512de01f88efdb6
Narges Jahromi <nrgs.jahromi@gmail.com>
2024-09-04 04:29:14 +0330

    change store URL

--- src/components/pages/profile/QRCodeInfo.tsx
+++ src/components/pages/profile/QRCodeInfo.tsx
@@ -34,7 +34,7 @@ const QRCodeInfo: React.FC<Props> = ({ storeData }) => {
       <Box className="space-y-2 m-0 flex flex-col justify-end items-center">
         <QRCodeCanvas
           ref={qrRef}
-          value={`https://scanbuy-store.liara.run/${storeData.store_id}`}
+          value={`https://scanbuy.liara.run/${storeData.store_id}`}
           title={"Title for my QR Code"}
           size={200}
           bgColor={"#ffffff"}
@@ -68,7 +68,7 @@ const QRCodeInfo: React.FC<Props> = ({ storeData }) => {
         id="outlined-start-adornment"
         size="small"
         fullWidth
-        value={`scanbuy-store.liara.run/${storeData.store_id}`}
+        value={`scanbuy.liara.run/${storeData.store_id}`}
         sx={{ direction: "ltr" }}
         InputProps={{
           startAdornment: (
