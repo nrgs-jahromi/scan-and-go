bad84ed691e4f11099019cf4c37c2d2c93cfe69b
Narges Jahromi <nrgs.jahromi@gmail.com>
2024-09-08 00:31:59 +0330

    change QRCode URL

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 // export const API_BASE_URL = "http://127.0.0.1:8000";
-export const API_BASE_URL = "https://scanbuy-backend.liara.run/";
+// export const API_BASE_URL = "https://scanbuy-backend.liara.run/";
+export const API_BASE_URL = "https://scanbuy-back.liara.run/";
 
 export const fetcher = axios.create({
   baseURL: API_BASE_URL,
